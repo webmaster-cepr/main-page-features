8164a18b85d49abc740bb03194c533a139c9d7e5
mattsedlar <mattsedlar@thegeekticket.com>
2013-08-28 16:46:03 -0400

    jQuery call in default.php

---
+++ tmpl/default.php
@@ -0,0 +1,10 @@
+<?php
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+$document = JFactory::getDocument();
+$document->addScript('http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+
+?>
+
